35a6a97f8b8a6847387f1ed9eb47403cf0051ae5
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-08-15 12:50:03 -0400

    Adicioando as telas da car_page, de preço, detalhe e dados
    Adicionei as telas de Preço e detalhe e dados dentro do arquivo da Car_page

--- lib/pages/car_page.dart
+++ lib/pages/car_page.dart
@@ -1,3 +1,6 @@
+import 'package:appcarro/pages/car_page_widgets/Dados_page.dart';
+import 'package:appcarro/pages/car_page_widgets/Detalhe_page.dart';
+import 'package:appcarro/pages/car_page_widgets/Preco_page.dart';
 import 'package:flutter/material.dart';
 
 class CarPage extends StatelessWidget {
@@ -5,8 +8,45 @@ class CarPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Tela de carro em construção'),
+    return const DefaultTabController(
+        length: 3,
+        child: Column(
+          children: [
+            TabBar(tabs: [
+              Tab(text: 'Dados'),
+              Tab(text: "Detelhes"),
+              Tab(text: 'Preço')
+            ]),
+            Expanded(
+                child: TabBarView(children: [
+                  //Pagina de Dados com seus devidos paremetros
+                    CarDadosPage(
+                        marca: "Ferrari",
+                        ano: "2026",
+                        tipo: "Eletronico",
+                        quilometragem: "0 Km",
+                        cambio: "Automatico",
+                        placa: "xl44r8",
+                        combustivel: "flex"
+                    ),
+                    // Tela de Detalhe com seus devidos paremetros
+                    CarDetalhePage(
+                        estado: "Com um pequeno arranhado no parabrisa",
+                        acabamento: "Bancos feitos a couro de jabuti",
+                        acessorio: "Boneco funco pop colado no painel",
+                        proprietario: "Jorge Amaral",
+                        troca: "Sim",
+                        infoOpiconal: "As vezes da umas engasgadas na hora de ligar"
+                    ),
+                    // Tela de Preço com seu parematros
+                    CarPrecoPage(preco: "R\$3999")
+                  /// TODOS OS PAREMATROS SÃO OBRIGATORIOS
+                  ]
+                )
+            ),
+          ],
+        )
     );
   }
 }
+
