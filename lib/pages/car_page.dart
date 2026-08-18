8944b46e62476b16376033cf4294565e064d96e3
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-08-17 21:00:49 -0400

    Implement tabbed interface for car details
    Replaced placeholder text with a tabbed interface for car details, including data, details, and price pages. Added a button to navigate to the car registration page.

--- lib/pages/car_page.dart
+++ lib/pages/car_page.dart
@@ -1,3 +1,7 @@
+import 'package:appcarro/pages/car_page_widgets/Castro_page.dart';
+import 'package:appcarro/pages/car_page_widgets/Dados_page.dart';
+import 'package:appcarro/pages/car_page_widgets/Detalhe_page.dart';
+import 'package:appcarro/pages/car_page_widgets/Preco_page.dart';
 import 'package:flutter/material.dart';
 
 class CarPage extends StatelessWidget {
@@ -5,8 +9,70 @@ class CarPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Tela de carro em construção'),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Column(
+          children: [
+            const TabBar(tabs: [
+              Tab(text: 'Dados'),
+              Tab(text: "Detelhes"),
+              Tab(text: 'Preço')
+            ]),
+
+            const Expanded(
+                child: TabBarView(children: [
+//Pagina de Dados com seus devidos paremetros
+
+              CarDadosPage(
+                  marca: "Ferrari",
+                  ano: "2026",
+                  tipo: "Eletronico",
+                  quilometragem: "0 Km",
+                  cambio: "Automatico",
+                  placa: "xl44r8",
+                  combustivel: "flex"),
+
+// Tela de Detalhe com seus devidos paremetros
+
+              CarDetalhePage(
+                  estado: "Com um pequeno arranhado no parabrisa",
+                  acabamento: "Bancos feitos a couro de jabuti",
+                  acessorio: "Boneco funco pop colado no painel",
+                  proprietario: "Jorge Amaral",
+                  troca: "Sim",
+                  infoOpiconal: "As vezes da umas engasgadas na hora de ligar"),
+
+// Tela de Preço com seu parematros
+
+              CarPrecoPage(preco: "R\$3999")
+
+              /// TODOS OS PAREMATROS SÃO OBRIGATORIOS
+            ]
+                )
+            ),
+            SizedBox(
+              height: 90, // Define uma altura customizada para o botão
+              child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: FilledButton.icon(
+                    onPressed: () {
+                      // Navega para a página de formulário de contato
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const CarCadastroPage(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.mail_sharp),
+                    label: const Text(
+                      'cadastrar novo carro',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+              )
+            ),
+          ],
+        ));
   }
 }
